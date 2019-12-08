de225e428c88651e51a73c3ae34979c100b90b94
Mustafa Berk Yaldiz <berk17@gmail.com>
2019-12-08 19:53:09 +0900

    revert

--- mystory/mystory/settings.py
+++ mystory/mystory/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "/opt/bitnami/apps/django/django_projects/vocastory/mystory/static"),
-    #'/var/www/static/',
+    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "/opt/bitnami/apps/django/django_projects/vocastory/mystory/static"),
 ]
 
 # Modified for login/logout redirection
