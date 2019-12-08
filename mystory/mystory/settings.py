34205797da4f6bb6c9adb66ad5d134651a014203
Mustafa Berk Yaldiz <berk17@gmail.com>
2019-12-08 19:38:05 +0900

    fix

--- mystory/mystory/settings.py
+++ mystory/mystory/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/opt/bitnami/apps/django/django_projects/vocastory/mystory/static"),
     #'/var/www/static/',
 ]
 
